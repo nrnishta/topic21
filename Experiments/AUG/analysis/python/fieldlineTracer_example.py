00e8e0d77d31bbca1933a6ad6d98b78ab7a744a7
Nicola Vianello <nicola.vianello@igi.cnr.it>
2017-07-14 16:52:12 +0200

    added the plot and the figures for the field line tracing

--- Experiments/AUG/analysis/python/fieldlineTracer_example.py
+++ Experiments/AUG/analysis/python/fieldlineTracer_example.py
@@ -1,75 +1,87 @@
-#!/usr/bin/env python
-
 """
-Example script running a field line tracer
-
-Nick Walkden, May 2015
+Script to compute the appropriate Lparallel using cythonized version
+of FLT code. We will compute the parallel connection length from the
+height of magnetic axis and a series of points for which we will compute
+both the parallel connection length from midplane to LFS and from
+Xpoint height to the LFS. We will also save the distance as R-Rsep and
+rho_p
 """
 
 from cyfieldlineTracer import get_fieldline_tracer
 from copy import deepcopy as copy
 import numpy as np
-
-#loading from a MAST shot
-my_tracer =  get_fieldline_tracer('RK4', gfile='../equilibriadata/Shot34102_3s.g',
-                                  revBt=True, interp='quintic') #RK4Tracer(gfile='/home/nwalkden/gfiles/sxd2010.geqdsk')
-
-# The tracer is now ready to be used. Note that you can access
-# equilibrium quantities using my_tracer.eq which is an equilibrium object
-
-# To trace a field line, call the trace method
-my_fieldline = my_tracer.trace(1.36,0.0,mxstep=10000,ds=1e-2,tor_lim=20.0*np.pi)
-# Here the first two arguments are the R and Z starting points 
-# mxstep is the maximum number of steps to take
-# ds is the distance between each step along the field line
-
+import eqtools
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-fig = plt.figure()
+shotList = (34102, 34103, 34104, 34105, 34106)
+for shot in shotList:
+    Eq = eqtools.AUGDDData(shot)
+    Eq.remapLCFS()
+    
+    # load now the tracer for the same shot all at 3s
+    myTracer = get_fieldline_tracer('RK4', machine='AUG', shot=shot, time=3, interp='quintic')
+    # height of magnetic axis
+    zMAxis = myTracer.eq.axis.__dict__['z']
+    # height of Xpoint
+    zXPoint = myTracer.eq.xpoints['xpl'].__dict__['z']
+    # now determine at the height of the zAxis the R of the LCFS
+    _idTime = np.argmin(np.abs(Eq.getTimeBase()-3))
+    RLcfs = Eq.getRLCFS()[_idTime, :]
+    ZLcfs = Eq.getZLCFS()[_idTime, :]
+    # onlye the part greater then xaxis
+    ZLcfs = ZLcfs[RLcfs > myTracer.eq.axis.__dict__['r']]
+    RLcfs = RLcfs[RLcfs > myTracer.eq.axis.__dict__['r']]
+    Rout = RLcfs[np.argmin(np.abs(ZLcfs[~np.isnan(ZLcfs)]-zMAxis))]
+    rmin = np.linspace(Rout+0.003, 2.19, num=25)
+    # this is R-Rsep
+    rMid = rmin-Rout
+    # this is Rho
+    rho = Eq.rz2psinorm(rmin, np.repeat(zMAxis, rmin.size), 3, sqrt=True)
+    # now create the Traces and lines
+    fieldLines = [myTracer.trace(r, zMAxis, mxstep=100000, ds=1e-2, tor_lim=20.0*np.pi) for r in rmin]
+    # create and save the figure
+    fig = plt.figure(figsize=(18, 10))
+    ax1 = fig.add_subplot(121,aspect='equal')
+    # plot the separatrix
+    ax1.contour(myTracer.eq.R, myTracer.eq.Z, myTracer.eq.psiN, [1], linestyles='--', colors='k', lw=2)
+    ax1.plot(myTracer.eq.wall['R'],myTracer.eq.wall['Z'],'k', lw=3)
+    for line in fieldLines:
+        ax1.plot(line.R,line.Z,'-', lw=0.7)
 
-# Rotating the field line in the toroidal direction is done
-# via the method rotateToroidal
-my_fieldline_rotated = my_tracer.trace(1.425,0.0,mxstep=100000,ds=1e-2)#copy(my_fieldline)
-my_fieldline_rotated.rotate_toroidal(np.pi)
+    ax2 = fig.add_subplot(122,projection='3d',aspect='equal')
+    for line in fieldLines:
+        ax2.plot(line.X,line.Y,line.Z,'-', lw=0.7)
 
-ax1 = fig.add_subplot(121,aspect='equal')
-ax1.plot(my_fieldline.R,my_fieldline.Z,'-ro')
-ax1.plot(my_tracer.eq.wall['R'],my_tracer.eq.wall['Z'],'k')
+    ax2.set_zlim3d(-2.0,2.0)
+    ax2.set_xlim3d(-2.0,2.0)
+    ax2.set_ylim3d(-2.0,2.0)
+    mpl.pylab.savefig('../pdfbox/Shot%5i' % shot +'_fieldline.pdf', bbox_to_inches='tight')
 
-ax2 = fig.add_subplot(122,projection='3d',aspect='equal')
+    
+    
+    fieldLinesZ = [line.filter('Z', [-1, zXPoint]) for line in fieldLines]
+    Lpar =np.array([])
+    for line in fieldLinesZ:
+        try:
+            _dummy = np.abs(line.S[0] - line.S[-1])
+        except:
+            _dummy = np.nan
+        Lpar = np.append(Lpar, _dummy)
 
-ax2.plot(my_fieldline.X,my_fieldline.Y,my_fieldline.Z,'r')
-ax2.plot(my_fieldline_rotated.X,my_fieldline_rotated.Y,my_fieldline_rotated.Z,'g')
+    # compute also the other one
+    LparAll = np.array([])
+    for line in fieldLines:
+        try:
+            _dummy = np.abs(line.S[0] - line.S[-1])
+        except:
+            _dummy = np.nan
+        LparAll = np.append(LparAll, _dummy)
 
-#Now apply filtering to remove parts of the fieldline not wanted
+    np.savetxt('../equilibriadata/LparallelShot%5i' % shot+'.txt', np.c_[rMid, rho, LparAll, Lpar])
 
-my_filtered_fieldline = my_fieldline.filter('phi',[0.0,np.pi])
-ax2.plot(my_filtered_fieldline.X,my_filtered_fieldline.Y,my_filtered_fieldline.Z,'b')
-ax1.plot(my_filtered_fieldline.R,my_filtered_fieldline.Z,'-bx')
 
-ax2.set_zlim3d(-2.0,2.0)
-ax2.set_xlim3d(-2.0,2.0)
-ax2.set_ylim3d(-2.0,2.0)
-plt.show()
 
-#Get the connection length
-
-#Get Radius and parallel distance, S, at both ends of the fieldline
-
-S0,S1 = my_fieldline.S[0],my_fieldline.S[-1]
-R0,R1 = my_fieldline.R[0],my_fieldline.R[-1]
-
-print('R = '+str(R0)+', S = '+str(S0))
-print('R = '+str(R1)+', S = '+str(S1))
-
-#Note this works because we started the trace at the midplane, so S = 0 corresponds to the midplane
-#If we wanted to do, for example, the X-point to target connection length we could do
-
-new_fieldline = my_fieldline.filter('Z',[-10.0,-1.47]) #Get just the bit of the fieldline below the X-point
-
-Lpar = np.abs(new_fieldline.S[0] - new_fieldline.S[-1])
-
-print(Lpar)
 
 
