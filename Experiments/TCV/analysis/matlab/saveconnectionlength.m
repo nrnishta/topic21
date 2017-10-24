0b451ba5a4e73a8708e895c4cc6debb3a1f95ce4
vianello <vianello@lac.epfl.ch>
2017-10-24 17:11:56 +0200

    update to pulse file to take care of the old shots

--- Experiments/TCV/analysis/matlab/saveconnectionlength.m
+++ Experiments/TCV/analysis/matlab/saveconnectionlength.m
@@ -3,31 +3,39 @@
 % time basis of LP probes for each of the shots 
 % analyzed
 
-shotList = [57418, 57425, 57437, 57450, 57454, 57459, 57461, 57497]
+shotList = [51181 51182 51358 52062 ...
+            52065 52212 52629 52633 52644 52650 52766 52767 ...
+            53514 53516 53518 53520 53562 53564 53565 53569 ...
+            53571 53573 53575 53577 53579 53582 54867 54868 ...
+            54869 54870 54873 54874 54876];
 for shot = 1:length(shotList)
-    % get the time basis
-    h = mdsopen('tcv_shot', shotList(shot));
-    time = mdsvalue('\results::langmuir:time');
-    % compute the first one to get the number in dr_us
-    h = sol_geometry(shotList(shot), time(1), 'dR_us', 0.03);
-    drUs = h.dr_us;
-    lParUp = zeros(length(time), length(drUs));
-    lParDiv = zeros(length(time), length(drUs));
-    lParUp(1, :) = h.cl_lfs;
-    lParDiv(1, :) = h.cl_div_lfs;
-    close all
-    for t = 2:length(time)
-        try
-            h =sol_geometry(shotList(shot), time(t), 'dR_us', 0.03);
-            % we need to ensure they have the same x basis
-            lParUp(t, :) = interp1(h.dr_us, h.cl_lfs, drUs, 'spline');
-            lParDiv(t, :) = interp1(h.dr_us, h.cl_div_lfs, drUs, 'spline');
+        disp(['Working on shot ' num2str(shotList(shot))])
+        % get the time basis
+        h = mdsopen('tcv_shot', shotList(shot));
+        [time, status] = mdsvalue('\results::langmuir:time');
+        if mod(status, 2) == 1
+            % compute the first one to get the number in dr_us
+            h = sol_geometry(shotList(shot), time(1), 'dR_us', 0.03);
+            drUs = h.dr_us;
+            lParUp = zeros(length(time), length(drUs));
+            lParDiv = zeros(length(time), length(drUs));
+            lParUp(1, :) = h.cl_lfs;
+            lParDiv(1, :) = h.cl_div_lfs;
             close all
-        catch
-            warning('Sol Geometry does not work for this time')
+            for t = 2:length(time)
+                try
+                    h =sol_geometry(shotList(shot), time(t), 'dR_us', 0.03);
+                    % we need to ensure they have the same x basis
+                    lParUp(t, :) = interp1(h.dr_us, h.cl_lfs, drUs, 'spline');
+                    lParDiv(t, :) = interp1(h.dr_us, h.cl_div_lfs, drUs, 'spline');
+                    close all
+                catch
+                    warning('Sol Geometry does not work for this time')
+                end
+            end
+            save(['../data/connectionlength' num2str(shotList(shot)) 'mat'], ...
+                 'drUs', 'time', 'lParDiv', 'lParUp')
         end
-    end
-    save(['../data/connectionlength' num2str(shotList(shot)) 'mat'], ...
-         'drUs', 'time', 'lParDiv', 'lParUp')
+        mdsclose
 end
     
--- Experiments/TCV/analysis/python/createpulsefile.py
+++ Experiments/TCV/analysis/python/createpulsefile.py
@@ -1,7 +1,14 @@
 import topic21Mds
 
-shotL = (57418, 57425, 57437, 57450, 57454, 57459, 57461, 57497)
+shotL = (51080, 51084, 51124, 51126, 51130, 51132, 51133, 51134, 51138, 51139,
+         51141, 51176, 51358, 52062, 52065, 52212, 52629, 52633, 52644, 52650,
+         52766, 52767, 53514, 53516, 53518, 53520, 53562, 53564, 53565, 53569,
+         53571, 53573, 53575, 53577, 53579, 53582, 54867, 54868,
+         54869, 54870, 54873, 54874, 54876)
 for shot in shotL:
-    Out = topic21Mds.Tree(shot)
-    Out.toMds()
-    print('Written pulse file for shot %5i' % shot)
+    try:
+        Out = topic21Mds.Tree(shot)
+        Out.toMds()
+        print('Written pulse file for shot %5i' % shot)
+    except:
+        print('Pulse file not written for shot %5i' % shot)
