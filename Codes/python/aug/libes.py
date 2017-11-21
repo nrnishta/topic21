fc0e69b2718eab463f849c8425c96fb4d7640bcb
Nicola Vianello <nicola.vianello@igi.cnr.it>
2017-11-21 17:38:36 +0100

    libes class now contains the computation of amplitude and location as in JET

--- Codes/python/aug/libes.py
+++ Codes/python/aug/libes.py
@@ -54,17 +54,22 @@ class Libes(object):
                         (self.time <= trange[1]))[0]
 
         ne = self.ne[_idx, :]
+        neN = self.neNorm[_idx, :]
         if interelm:
             print('Computing inter-ELM profiles')
             self._maskElm(trange=trange, **kwargs)
             ne = ne[self._interElm, :]
+            neN = neN[self._interElm, :]
         if elm:
             self._maskElm(trange=trange, **kwargs)
             print('Computing ELM profiles')
             ne = ne[self._Elm, :]
+            neN = neN[self._Elm, :]
 
         profiles = np.nanmean(ne, axis=0)
         error = np.nanstd(ne, axis=0)
+        profilesN = np.nanmean(neN, axis=0)
+        errorN = np.nanstd(neN, axis=0)
         # now build the appropriate Efolding length on the
         # recomputed profiles using an UnivariateSpline
         # interpolation weighted on the error
@@ -72,8 +77,52 @@ class Libes(object):
         # 
         S = UnivariateSpline(Rmid, profiles, w=1./error, s=0)
         Efold = np.abs(S(Rmid)/S.derivative()(Rmid))
-        return profiles, error, Efold
+        return profiles, error, Efold, profilesN, errorN
 
+    def amplitudeShoulder(self, dt=0.1, reference=[1.5, 1.6], start=1.6):
+        """
+        Compute the amplitude and location of the shoulder defined as
+        the difference between normalized profiles in the SOL w.r.t.
+        a reference profile. It also compute the location in term of
+        normalized poloidal flux and Rmid of the maximum of the shoulder
+
+        Parameters
+        ----------
+        dt : floating
+            resolution in [s]. If given compute the evolution with the
+            given time resolution
+        reference : 2D floating 
+            It contains the time interval computed for the reference
+            profile
+        start : floating
+            Starting point for the evaluation of the amplitude 
+        Returns
+        -------
+        timebasis, amplitude (only in the SOL), location
+        """
+        # first compute the normalized reference profile
+        _, _, _, pN, eN = self.averageProfile(trange=reference)
+        # limit to the region for rho > 1
+        _idx = np.where((self.time > start))[0]
+        _npoint = int(np.floor((self.time[_idx].max()-self.time[_idx].min())/dt))
+        Amplitude = []
+        Location = []
+        time = np.zeros(_npoint)
+        rhoDummy = self.rho[self.rho>=1]
+        pN = pN[self.rho >= 1]
+        # number of point given the resolution
+        
+        for i in range(_npoint):
+            _, _, _, pDummy, eDummy = self.averageProfile(trange=[start+i*dt, start+(i+1)*dt])
+            pDummy = pDummy[self.rho>=1]
+            Amplitude.append(pDummy-pN)
+            Location.append(rhoDummy[np.argmax(pDummy-pN)])
+            time[i] = (start+i*dt+dt/2)
+            
+        Amplitude = np.asarray(Amplitude)
+        Location = np.asarray(Location)
+
+        return rhoDummy, time, Amplitude, Location
 
     def _maskElm(self, usedda=False, threshold=3000, trange=[2, 3],
                  check=False):
