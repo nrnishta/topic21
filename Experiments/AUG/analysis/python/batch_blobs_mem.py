eac71fcaef43096aa38d207abda19c9c258106f2
Nicola Vianello <nicola.vianello@igi.cnr.it>
2017-11-21 14:33:26 +0100

    update to blob analysis

--- Experiments/AUG/analysis/python/batch_blobs_mem.py
+++ Experiments/AUG/analysis/python/batch_blobs_mem.py
@@ -3,109 +3,32 @@ import augFilaments
 import numpy as np
 df = pd.read_csv('../data/MEM_Topic21.csv')
 shotList = df['shot'].values
-shotList = shotList[shotList> 34279]
+shotList = shotList[shotList> 34107]
 for shot in shotList:
-    # first load
-    Data = augFilaments.Filaments(shot, Xprobe=df['X'][df['shot'] == shot].values)
-    # first interval
-    if shot <= 34108:
-        # first one
-        tmin = df['tmin1'][df['shot'] == shot].values
-        tmax = df['tmax1'][df['shot'] == shot].values
-        out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                block=[0.015, 1.5], normalize=True, detrend=True,
-                                trange=[tmin, tmax])
-        out.to_netcdf('../data/Shot%5i' % shot + '_1Stroke.nc')
-        # second one
-        tmin = df['tmin2'][df['shot'] == shot].values
-        tmax = df['tmax2'][df['shot'] == shot].values
-        out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                block=[0.015, 1.5], normalize=True, detrend=True,
-                                trange=[tmin, tmax])
-        out.to_netcdf('../data/Shot%5i' % shot + '_2Stroke.nc')
-        # third one
-        tmin = df['tmin3'][df['shot'] == shot].values
-        tmax = df['tmax3'][df['shot'] == shot].values
-        out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                block=[0.015, 1.5], normalize=True, detrend=True,
-                                trange=[tmin, tmax])
-        out.to_netcdf('../data/Shot%5i' % shot + '_3Stroke.nc')
-        # 4 stroke
-        if not np.isnan(df['tmin4'][df['shot'] == shot].values):
-            tmin = df['tmin4'][df['shot'] == shot].values
-            tmax = df['tmax4'][df['shot'] == shot].values
-            out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                    block=[0.015, 1.5], normalize=True, detrend=True,
-                                    trange=[tmin, tmax])
-            out.to_netcdf('../data/Shot%5i' % shot + '_4Stroke.nc')
-        # 5 stroke
-        if not np.isnan(df['tmin5'][df['shot'] == shot].values):
-            tmin = df['tmin5'][df['shot'] == shot].values
-            tmax = df['tmax5'][df['shot'] == shot].values
-            out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                    block=[0.015, 1.5], normalize=True, detrend=True,
-                                    trange=[tmin, tmax])
-            out.to_netcdf('../data/Shot%5i' % shot + '_5Stroke.nc')
-        # 6 stroke
-        if not np.isnan(df['tmin6'][df['shot'] == shot].values):
-            tmin = df['tmin6'][df['shot'] == shot].values
-            tmax = df['tmax6'][df['shot'] == shot].values
-            out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                    block=[0.015, 1.5], normalize=True, detrend=True,
-                                    trange=[tmin, tmax])
-            out.to_netcdf('../data/Shot%5i' % shot + '_6Stroke.nc')
-    # H-Mode
-    else:
-        # first one
-        tmin = df['tmin1'][df['shot'] == shot].values
-        tmax = df['tmax1'][df['shot'] == shot].values
-        out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                block=[0.015, 1.5], normalize=True, detrend=True,
-                                trange=[tmin, tmax], interELM=True, threshold=500)
-        out.to_netcdf('../data/Shot%5i' % shot + '_1Stroke.nc')
-        # second one
-        tmin = df['tmin2'][df['shot'] == shot].values
-        tmax = df['tmax2'][df['shot'] == shot].values
-        out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                block=[0.015, 1.5], normalize=True, detrend=True,
-                                trange=[tmin, tmax], interELM=True, threshold=500)
-        out.to_netcdf('../data/Shot%5i' % shot + '_2Stroke.nc')
-        # third one
-        tmin = df['tmin3'][df['shot'] == shot].values
-        tmax = df['tmax3'][df['shot'] == shot].values
-        out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                block=[0.015, 1.5], normalize=True, detrend=True,
-                                trange=[tmin, tmax], interELM=True, threshold=500)
-        out.to_netcdf('../data/Shot%5i' % shot + '_3Stroke.nc')
-        # 4 stroke
-        if not np.isnan(df['tmin4'][df['shot'] == shot].values):
-            tmin = df['tmin4'][df['shot'] == shot].values
-            tmax = df['tmax4'][df['shot'] == shot].values
-            out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                    block=[0.015, 1.5], normalize=True, detrend=True,
-                                    trange=[tmin, tmax], interELM=True, threshold=500)
-            out.to_netcdf('../data/Shot%5i' % shot + '_4Stroke.nc')
-        # 5 stroke
-        if not np.isnan(df['tmin5'][df['shot'] == shot].values):
-            tmin = df['tmin5'][df['shot'] == shot].values
-            tmax = df['tmax5'][df['shot'] == shot].values
-            if shot == 34279:
-                lim = 600
+    D = df[df['shot'] == shot]
+    # first load the augFilaments data
+    Data = augFilaments.Filaments(shot, Xprobe=D['X'].values)
+    # then iterate with check on the number of strokes
+    for strokes in ('1', '2', '3', '4', '5', '6'):
+        # this is the check in case the timing is defined
+        if not np.isnan(D['tmin'+strokes].values[0]):
+            print('Evaluating Data for shot %5i strokes number %1i', shot, int(strokes))
+            tmin = D['tmin'+strokes].values[0]
+            tmax = D['tmax'+strokes].values[0]
+            # now distinguish the case with interELM or ELM
+            if np.isnan(D['Threshold_' + strokes].values[0]):
+                # no interelm
+                print('Evaluating without ELM for shot %5i strokes number %1i', shot, int(strokes))
+                out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
+                                        block=[0.015, 1.5], normalize=True, detrend=True,
+                                        trange=[tmin, tmax])
+                out.to_netcdf('../data/Shot%5i' % shot + '_'+strokes+'Stroke.nc')
             else:
-                lim = 300
-            out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                    block=[0.015, 1.5], normalize=True, detrend=True,
-                                    trange=[tmin, tmax], interELM=True, threshold=lim)
-            out.to_netcdf('../data/Shot%5i' % shot + '_5Stroke.nc')
-        # 6 stroke
-        if not np.isnan(df['tmin6'][df['shot'] == shot].values):
-            tmin = df['tmin6'][df['shot'] == shot].values
-            tmax = df['tmax6'][df['shot'] == shot].values
-            if shot == 34279:
-                lim = 600
-            else:
-                lim = 300
-            out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
-                                    block=[0.015, 1.5], normalize=True, detrend=True,
-                                    trange=[tmin, tmax], interELM=True, threshold=lim)
-            out.to_netcdf('../data/Shot%5i' % shot + '_6Stroke.nc')
+                print('Evaluating with ELM for shot %5i strokes number %1i', shot, int(strokes))
+                thr = D['Threshold_'+strokes].values[0]
+                out = Data.blobAnalysis(Probe='Isat_m06', otherProbe=['Isat_m10', 'Isat_m07'],
+                                        block=[0.015, 1.5], normalize=True, detrend=True,
+                                        trange=[tmin, tmax], interelm=True, threshold=thr)
+                out.to_netcdf('../data/Shot%5i' % shot + '_'+strokes+'Stroke.nc')
+        else:
+            print('No strokes %1i for shot %5i', int(strokes), shot)
