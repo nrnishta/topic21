52e3bf7ebf9a04df2ab0073c56048072300fab76
vianello <vianello@lac.epfl.ch>
2017-12-06 15:23:20 +0100

    script for the creation of new pulsefiles

---
+++ Codes/python/tcv/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import division
+
+from .gas import *
+from .boloradiation import *
+from .gauges import *
+from .langmuir import *
+from .tcvFilaments import *
+from .tcvProfiles import *
--- Experiments/TCV/analysis/python/createpulsefile.py
+++ Experiments/TCV/analysis/python/createpulsefile.py
@@ -1,7 +1,6 @@
 import topic21Mds
 
-shotL = (58608, 58610, 58611, 58614, 58623, 58624,
-         58629, 58635, 58637, 58639)
+shotL = (57082, 57086, 57087, 57088, 57089)
 for shot in shotL:
     try:
         Out = topic21Mds.Tree(shot)
