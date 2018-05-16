3218467515973202503143c9137113d19addd31b
Nicola Vianello <nicola.vianello@gmail.com>
2018-05-17 00:04:00 +0200

    update script for comparison
    Now should handle home directory and add the script for the evolution of quantity for constant Q95

--- Experiments/Comparison/python/scriptAnalysis.py
+++ Experiments/Comparison/python/scriptAnalysis.py
@@ -5,12 +5,16 @@ import xarray as xray
 import h5py
 from scipy.interpolate import UnivariateSpline, interp1d
 import pandas as pd
+from os.path import expanduser
 mpl.rcParams['font.family'] = 'sans-serif'
 mpl.rc("font", size=18)
 mpl.rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
 mpl.rc("lines", linewidth=2)
+homeDir = expanduser("~")
+EuroFusionDir = homeDir + "/Dropbox/Work/Collaboration/EuroFusion/"
 Data = pd.read_csv(
-    '~/Desktop/Topic-21/Experiments/TCV/data/BlobDatabase.csv')
+    EuroFusionDir +
+    'MST1-2017-2018/Topic-21/Repository/Experiments/TCV/data/BlobDatabase.csv')
 DataTCV = Data[((Data['Rho'] > 1.025) &
                 (Data['Rho'] <= 1.05) &
                 (Data['Conf'] == 'LSN') &
@@ -297,7 +301,8 @@ while loop:
         ipListT = ('0.185', '0.24', '0.34')
         for shot, t, c, _ip in zip(shotListA, tListA, colorList, ipListA):
             # open the HDF file
-            DirectoryAug = '/Users/vianello/Documents/Fisica/Conferences/IAEA/iaea2018/data/aug/'
+            DirectoryAug = homeDir+"/Documents/" \
+                           "Fisica/Conferences/IAEA/iaea2018/data/aug/"
             File = h5py.File(DirectoryAug + 'Shot%5i' % shot + '.h5', 'r')
             neLBtime = File['timeLiB'].value
             # limit to the interval
@@ -374,7 +379,7 @@ while loop:
             text.set_color(c)
             handle.set_visible(False)
         # Now TCV
-        DirectoryTcv = "/Users/vianello/Documents/Fisica/Conferences/" \
+        DirectoryTcv = homeDir + "/Documents/Fisica/Conferences/" \
                        "IAEA/iaea2018/data/tcv/"
         File = h5py.File(DirectoryTcv + 'ProfilesConstantBt.h5', 'r')
         for shot, col, _ip in zip(
@@ -1163,6 +1168,178 @@ while loop:
         fig3.savefig('../pdfbox/AmplitudeVsLambdaConstantBt.pdf',
                      bbox_to_inches='tight')
 
+    elif selection == 11:
+        shotListA = (34103, 34102, 34104)
+        colorList = ('#01406C', '#F03C07', '#28B799')
+        iPAug = (0.6, 0.8, 1)
+        DirectoryAug = homeDir + "/Documents/Fisica/"\
+                       "Conferences/IAEA/iaea2018/data/aug/"
+        # this is the plot with amplitude, target vs H-5
+        fig, ax = mpl.pylab.subplots(figsize=(10, 9), nrows=3,
+                                     ncols=1, sharex=True)
+        fig.subplots_adjust(hspace=0.08,
+                            bottom=0.15, left=0.15,
+                            right=0.98)
+        # this is the plot of the amplitude, target vs nGw
+        fig2, ax2 = mpl.pylab.subplots(figsize=(10, 9),
+                                       nrows=3, ncols=1,
+                                       sharex=True)
+        fig2.subplots_adjust(hspace=0.08,
+                             bottom=0.15, left=0.15, right=0.98)
+        # this is the plot if shoulder vs LambdaDiv
+        fig3, ax3 = mpl.pylab.subplots(figsize=(10, 6),
+                                       nrows=2, ncols=1, sharex=True)
+        fig3.subplots_adjust(hspace=0.08,
+                             bottom=0.15, left=0.15, right=0.98)
+
+        for i, (shot, ip, col) in enumerate(zip(shotListA, iPAug, colorList)):
+            df = xray.open_dataarray(
+                '../../AUG/analysis/data/Shot%5i' % shot +
+                '_DensityRadiation.nc')
+            File = h5py.File(DirectoryAug + 'Shot{}.h5'.format(shot), 'r')
+            LiBN = File['LiBNorm'].value
+            rho = File['rhoLiB'].value
+            time = File['timeLiB'].value
+            # now integrate in the near and far SOL defining respectively for
+            # (1<rho<1.03 and 1.03<rho<1.06) wrt initial perio [1.2, 1.4]
+            _idx = np.where((time >= 1.2) & (time <= 1.4))[0]
+            Reference = np.nanmean(LiBN[_idx, :], axis=0)
+            errReference = np.nanstd(LiBN[_idx, :], axis=0)
+            LiBN = LiBN[np.where(time > 1.4)[0], :]
+            time = time[np.where(time > 1.4)[0]]
+            _npoint = int(np.floor((time.max()-time.min()))/0.02)
+            Split = np.asarray(np.array_split(LiBN, _npoint, axis=0))
+            Amplitude = np.asarray(
+                [np.nanmean(p, axis=0)-Reference for p in Split])
+            ErrAmplitude = np.asarray([
+                np.sqrt(np.power(np.nanstd(p, axis=0), 2) +
+                        np.power(errReference, 2)) for p in Split])
+            _ = np.asarray(np.array_split(time, _npoint))
+            timeSplit = np.asarray([np.mean(k) for k in _])
+            aNear = np.nanmean(Amplitude[:, np.where(
+                (rho >= 1) & (rho < 1.03))[0]], axis=1)
+            aFar = np.nanmean(Amplitude[:, np.where(
+                (rho >= 1.03) & (rho < 1.06))[0]], axis=1)
+            aNearS = np.nanstd(Amplitude[:, np.where(
+                (rho >= 1) & (rho < 1.03))[0]], axis=1)
+            aFarS = np.nanstd(Amplitude[:, np.where(
+                (rho >= 1.03) & (rho < 1.06))[0]], axis=1)
+            # we now limit all the quantities in netcdf to
+            # time greater than the minimum of
+            # the timeSplit and interpolate
+            _dummy = df.where((df.t >= timeSplit.min()) &
+                              (df.t <= timeSplit.max()), drop=True)
+            # this is the plot vs edge density
+            S = interp1d(
+                _dummy.t.values, _dummy.sel(sig='H-5').values,
+                kind='linear', fill_value='extrapolate')
+            ax[0].errorbar(S(timeSplit), aNear, yerr=aNearS,
+                           fmt='o', color=col,
+                           label=r'#%5i' % shot + ' %3.2f' % ip + ' MA')
+            ax[1].errorbar(S(timeSplit), aFar, yerr=aFarS, fmt='o', color=col,
+                           label=r'#%5i' % shot + ' %3.2f' % ip + ' MA')
+            ax[2].plot(_dummy.sel(sig='H-5'),
+                       _dummy.sel(sig='neTarget'), 'o', color=col,
+                       label=r'#%5i' % shot + ' %3.2f' % ip + ' MA')
+            # this is the plot vs greenwald
+            S = interp1d(
+                _dummy.t.values, _dummy.sel(sig='nGw').values,
+                kind='linear', fill_value='extrapolate')
+            ax2[0].errorbar(S(timeSplit), aNear, yerr=aNearS,
+                            fmt='o', color=col,
+                            label=r'#%5i' % shot + ' %3.2f' % ip + ' MA')
+            ax2[1].errorbar(S(timeSplit), aFar, yerr=aFarS, fmt='o', color=col,
+                            label=r'#%5i' % shot + ' %3.2f' % ip + ' MA')
+            ax2[2].plot(_dummy.sel(sig='nGw'),
+                        _dummy.sel(sig='neTarget'), 'o', color=col,
+                        label=r'#%5i' % shot + ' %3.2f' % ip + ' MA')
+
+            # now load the Lambda Div from saved h5 file
+            File = h5py.File(DirectoryAug+'Shot{}.h5'.format(shot))
+            _idx = np.where((File['LambdaDivRho'].value >= 1) &
+                            (File['LambdaDivRho'].value < 1.03))[0]
+            LNear = np.nanmean(File['LambdaDiv'].value[_idx, :],
+                               axis=0).ravel()
+            _idx = np.where((File['LambdaDivRho'].value >= 1.03) &
+                            (File['LambdaDivRho'].value < 1.06))[0]
+            LFar = np.nanmean(File['LambdaDiv'].value[_idx, :], axis=0).ravel()
+            LTime = File['LambdaDivTime'].value.ravel()
+            LNear = LNear[np.where((LTime >= timeSplit.min()) &
+                                   (LTime <= timeSplit.max()))[0]]
+            LFar = LFar[np.where((LTime >= timeSplit.min()) &
+                                 (LTime <= timeSplit.max()))[0]]
+            LTime = LTime[np.where((LTime >= timeSplit.min()) &
+                                   (LTime <= timeSplit.max()))[0]]
+
+            S = interp1d(LTime, LNear, kind='linear', fill_value='extrapolate')
+            ax3[0].errorbar(S(timeSplit), aNear,
+                            yerr=aNearS, fmt='o', color=col,
+                            label=r'#%5i' % shot + ' %3.2f' % ip + ' MA')
+            ax3[1].errorbar(S(timeSplit), aFar, yerr=aFarS, fmt='o', color=col,
+                            label=r'#%5i' % shot + ' %3.2f' % ip + ' MA')
+
+        ax[0].axes.get_xaxis().set_visible(False)
+        ax[1].axes.get_xaxis().set_visible(False)
+        ax[0].set_ylim([0, 1])
+        ax[1].set_ylim([0, 1])
+        ax[2].set_ylim([0, 1])
+        ax[2].set_xlim([0.5, 4])
+        ax[2].set_ylabel(r'n$^{peak}_{target} [10^{20}$m$^{-3}]$')
+        ax[0].set_ylabel(r'Amp [a.u]')
+        ax[0].text(0.5, 0.8, r'$1\leq \rho < 1.03$',
+                   transform=ax[0].transAxes)
+        ax[1].set_ylabel(r'Amp [a.u.]')
+        ax[1].text(0.5, 0.8, r'$1.03\leq \rho < 1.06$',
+                   transform=ax[1].transAxes)
+        ax[2].set_xlabel(r'n$_e^{Edge}[10^{20}$m$^{-2}]$')
+        leg = ax[0].legend(loc='best', numpoints=1, frameon=False, fontsize=14)
+        for text, color in zip(leg.get_texts(), colorList):
+            text.set_color(color)
+
+        ax2[0].axes.get_xaxis().set_visible(False)
+        ax2[1].axes.get_xaxis().set_visible(False)
+        ax2[0].set_ylim([0, 1])
+        ax2[1].set_ylim([0, 1])
+        ax2[2].set_ylim([0, 1])
+        ax2[2].set_xlim([0.25, 0.7])
+        ax2[2].set_ylabel(r'n$^{peak}_{target} [10^{20}$m$^{-3}]$')
+        ax2[0].set_ylabel(r'Amp [a.u]')
+        ax2[0].text(0.5, 0.8, r'$1\leq \rho < 1.03$',
+                    transform=ax2[0].transAxes)
+        ax2[1].set_ylabel(r'Amp [a.u.]')
+        ax2[1].text(0.5, 0.8, r'$1.03\leq \rho < 1.06$',
+                    transform=ax2[1].transAxes)
+        ax2[2].set_xlabel(r'n$_e$/n$_G$')
+        leg = ax2[0].legend(loc='best', numpoints=1,
+                            frameon=False, fontsize=14)
+        for text, color in zip(leg.get_texts(), colorList):
+            text.set_color(color)
+
+        ax3[0].axes.get_xaxis().set_visible(False)
+        ax3[0].set_ylim([0, 1])
+        ax3[1].set_ylim([0, 1])
+        ax3[1].set_xlim([0.1, 30])
+        ax3[0].set_xscale('log')
+        ax3[1].set_xscale('log')
+        ax3[0].set_ylabel(r'Amp [a.u]')
+        ax3[0].text(0.2, 0.3, r'$1\leq \rho < 1.03$',
+                    transform=ax3[0].transAxes)
+        ax3[1].set_ylabel(r'Amp [a.u.]')
+        ax3[1].text(0.2, 0.3, r'$1.03\leq \rho < 1.06$',
+                    transform=ax3[1].transAxes)
+        ax3[1].set_xlabel(r'$\Lambda_{div}$')
+        leg = ax3[0].legend(loc='best', numpoints=1,
+                            frameon=False, fontsize=14)
+        for text, color in zip(leg.get_texts(), colorList):
+            text.set_color(color)
+
+        fig.savefig('../pdfbox/AmplitudeTargetVsDensityConstantQ95.pdf',
+                    bbox_to_inches='tight')
+        fig2.savefig(
+            '../pdfbox/AmplitudeTargetVsGreenwaldConstantQ95.pdf',
+            bbox_to_inches='tight')
+        fig3.savefig('../pdfbox/AmplitudeVsLambdaConstantQ95.pdf',
+                     bbox_to_inches='tight')
     elif selection == 99:
         loop = False
     else:
