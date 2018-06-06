bd5564ca5a09e926cf26acfd6ce4b6e9d5004129
vianello <vianello@lac.epfl.ch>
2018-06-06 17:52:22 +0200

    continue the development of pressuredrop. Gaussian process is still too slow for target better to provide directly binned data. There are issues on the computation of the grid for row

--- Codes/python/tcv/pressuredrop.py
+++ Codes/python/tcv/pressuredrop.py
@@ -12,6 +12,10 @@ import langmuir
 import tcvProfiles
 from scipy.interpolate import interp1d
 import xarray
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import (RBF,
+                                              ConstantKernel,
+                                              WhiteKernel)
 
 class fluxpressure(object):
     def __init__(self, shot):
@@ -47,16 +51,17 @@ class fluxpressure(object):
         self.NeUpstream = self.Profile.profileNe(t_min=trange[0], t_max=trange[1], abscissa='sqrtpsinorm')
         # we are only interested in the SOL ma we retain part of the profile in the
         # confined region since in this way the gpr fit is more robust
-        _ = self.TeUpstream.remove_points((self.TeUpstream.X[:,0] < 0.9))
-        _ = self.NeUpstream.remove_points((self.NeUpstream.X[:,0] < 0.9))
+        _ = self.TeUpstream.remove_points((self.TeUpstream.X[:,0] < 0.95))
+        _ = self.NeUpstream.remove_points((self.NeUpstream.X[:,0] < 0.95))
         # GPR fit for both the quantities in a uniform rhogrid
-        self.rhoUpstream = np.linspace(0.9,1.15,50)
+        self.rhoUpstream = np.linspace(0.95,1.15,50)
         self.NeUFit, self.NeUFitErr, self.NeUGpr = self.Profile.gpr_robustfit(
             self.rhoUpstream, density=True, temperature=False,**kwargs)
         self.TeUFit, self.TeUFitErr, self.TeUGpr = self.Profile.gpr_robustfit(
             self.rhoUpstream, density=False, temperature=True,**kwargs)
-        self.PUpFit = constants.e * self.TeUFit * self.NeUFit * 1e20
-        self.PUpFitE = 1e20* constants.e * np.sqrt(
+        self.PUpFit = xarray.DataArray(constants.e * self.TeUFit * self.NeUFit * 1e20,
+                                       coords=[self.rhoUpstream],dims=['rho'])
+        self.PUpFit.attrs['Err'] = 1e20* constants.e * np.sqrt(
             np.power(self.TeUFit * self.NeUFitErr,2) +
             np.power(self.NeUFit * self.TeUFitErr,2))
         # for consistency we need also the raw data
@@ -65,11 +70,110 @@ class fluxpressure(object):
             _x = self.NeUpstream.X.ravel()
             _y = self.NeUpstream.y
             S = interp1d(_x[np.argsort(_x)],_y[np.argsort(_x)],kind='linear')
-            self.PUpRaw = 1e20* constants.e * self.TeUpstream.y * S(self.TeUpstream.X.ravel())
-            self.PUpRawE = 1e20 * constants.e * self.TeUpstream.err_y * S(self.TeUpstream.X.ravel())
+            self.PUpRaw = xarray.DataArray(
+                1e20* constants.e * self.TeUpstream.y * S(self.TeUpstream.X.ravel()),
+                coords=[self.TeUpstream.X.ravel()],dims=['rho'])
+            self.PUpRaw.attrs['Err'] = \
+                1e20 * constants.e * self.TeUpstream.err_y * S(self.TeUpstream.X.ravel())
         else:
             _x = self.TeUpstream.X.ravel()
             _y = self.TeUpstream.y
             S = interp1d(_x[np.argsort(_x)],_y[np.argsort(_x)],kind='linear')
-            self.PUpRaw = 1e20 * constants.e * self.NeUpstream.y * S(self.NeUpstream.X.ravel())
-            self.PUpRawE = 1e20 * constants.e * self.NeUpstream.err_y * S(self.NeUpstream.X.ravel())
+            self.PUpRaw = xarray.DataArray(
+                1e20 * constants.e * self.NeUpstream.y * S(self.NeUpstream.X.ravel()),
+                coords=[self.NeUpstream.X.ravel()], dims=['rho'])
+            self.PUpRaw.attrs['Err'] = \
+                1e20 * constants.e * self.NeUpstream.err_y * S(self.NeUpstream.X.ravel())
+
+        return self.PUpRaw, self.PUpFit
+
+    def _computeTarget(self, trange=None, **kwargs):
+        """
+
+        Parameters
+        ----------
+        trange: time range where computation of profiles at the lower target will be
+            evaluated
+        kwargs: keywords
+
+        Returns
+        -------
+
+        """
+        if not trange:
+            trange = [0.5, 0.7]
+
+        out = self.Target.UpStreamProfile(trange=trange)
+        _npoints = []
+        for key in ('en', 'r','rho','te'):
+            _npoints.append(np.size(out[key]))
+        for key in ('en', 'r','rho','te'):
+            out[key] = out[key][:np.min(_npoints)]
+
+
+        # for some reason there are cases where the number of points
+        # of density and temperature are different
+        # check if this is the case
+        # now we need to double check for anomalous high
+        # values of temperature or density
+        _ida = np.where((out['te'] < 100))[0]
+        _idb = np.where((out['en']/1e19 < 2))[0]
+        _id = np.unique(np.concatenate((_ida, _idb),axis=0))
+        for key in ('en', 'r','rho','te'):
+            print('formatting ' + key)
+            out[key] = out[key][_id]
+
+        neFit, neStd = self._gpfit(out['rho'],out['en']/1e19)
+        teFit, teStd = self._gpfit(out['rho'],out['te'])
+
+
+        # define the pressure both raw and Fit
+        self.PDoFit = xarray.DataArray(
+            constants.e * teFit * neFit * 1e19,
+            coords=[self.rhoUpstream],
+            dims=['rho']
+        )
+        _dummy = 1e19 * constants.e * np.sqrt(
+            np.power(teFit * neStd,2) +
+            np.power(neFit*teStd,2)
+        )
+        self.PDoFit.attrs['Err'] = _dummy
+
+        self.PDoRaw = xarray.DataArray(
+            constants.e * out['te'] * out['en'],
+            coords=[out['rho']],dims=['rho']
+        )
+
+        self.TeDownstream = xarray.DataArray(out['te'],coords=out['rho'],dims=['rho'])
+        self.NeDownstream = xarray.DataArray(out['ne'],coords=out['rho'],dims=['rho'])
+
+        return self.PDoRaw, self.PDoFit
+
+    def _gpfit(self, x, y):
+        """
+        Perform a gaussian process regression fit using a combination
+        of ConstantKernel,RBF and WhiteKernel. The new basis
+        will be the
+        Parameters
+        ----------
+        x: indipendent parameter of the fit
+        y: dependent parameter of the fit
+
+        Returns
+        -------
+
+        """
+
+        _dummy = np.vstack((x, y)).transpose()
+        _dummy = _dummy[~np.isnan(_dummy).any(1)]
+        x = _dummy[:, 0]
+        y = _dummy[:, 1]
+        X = np.atleast_2d(x)
+        kernel = ConstantKernel(0.5,(1e-3,1e3))*RBF(5,(1e-2,1e2)) + \
+            WhiteKernel(noise_level=1)
+        gp = GaussianProcessRegressor(kernel=kernel,n_restarts_optimizer=11)
+        gp.fit(X.T,y)
+        xN = np.atleast_2d(self.rhoUpstream)
+        yFit, sigma= gp.predict(xN.T,return_std=True)
+
+        return yFit, sigma
